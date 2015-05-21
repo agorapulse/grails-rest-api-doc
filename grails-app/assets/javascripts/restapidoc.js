ce0e6717476e1ed68929abb234bf7075ba6244ba
Benoit Hediard <ben@agorapulse.com>
2015-05-21 15:56:41 +0200

    Resources Grails Plugin replaced by Asset pipeline Grails Plugin

--- grails-app/assets/javascripts/restapidoc.js
+++ grails-app/assets/javascripts/restapidoc.js
@@ -1,4 +1,8 @@
-//=require restapidoc/handlebars.runtime-v3.0.0
-//=require restapidoc/jlinq
-//=require restapidoc/prettify
-//=require restapidoc/restapidoc-handlebars
+//= require jquery
+//= require bootstrap
+//= require restapidoc/handlebars.runtime-v3.0.0
+//= require restapidoc/jlinq
+//= require restapidoc/prettify
+//= require restapidoc/restapidoc-handlebars
+//= require_tree .
+//= require_self
---
+++ grails-app/views/layouts/restapidoc.gsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title><g:layoutTitle/></title>
+    <asset:javascript src="restapidoc.js"/>
+    <asset:stylesheet href="restapidoc.css"/>
+    <g:layoutHead/>
+</head>
+<body>
+<g:layoutBody/>
+<asset:javascript src="restapidoc/restapidoc.js"/>
+</body>
+</html>

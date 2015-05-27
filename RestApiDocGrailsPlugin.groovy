95b8610045b5beae824f42ff5194974a7b56e6b0
Benoit Hediard <ben@agorapulse.com>
2015-05-27 14:49:29 +0200

    [Release] Version bumped to 0.6.5.1-agorapulse

--- RestApiDocGrailsPlugin.groovy
+++ RestApiDocGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class RestApiDocGrailsPlugin {
     // the plugin version
-    def version = "0.6.5"
+    def version = "0.6.5.1-agorapulse"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
@@ -43,35 +43,5 @@ This plugin allows you to document methods (description, HTTP path/verb, paramet
 
     // Online location of the plugin's browseable source code.
     def scm = [ url: "https://github.com/loic911/restapidoc" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+    
 }
